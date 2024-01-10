0fa6d75eadbdb464a08ae0a163f5641d30ddf5bf
mahdi <mahdipanahimpui@gmail.com>
2024-01-10 13:59:21 +0330

    hello world view (for test)

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # created apps
+    'home.apps.HomeConfig',
+
     # third party apps
     'rest_framework',
 ]
